1966a283638ac043f23ada7c5d096ad27f73b6e8
hanching <mnshhps@gmail.com>
2026-09-19 16:33:22 +0800

    fix: lottery

--- main.js
+++ main.js
@@ -63,13 +63,3 @@ function drawSingle() {
     const randomIndex = Math.floor(Math.random() * candidates.length);
     document.getElementById('resultSingle').innerText = '抽出號碼：' + candidates[randomIndex] + ' 號';
 }
-
-function drawMulti() {
-    alert('   ');
-    document.getElementById('resultMulti').innerText = ' ';
-}
-
-function drawSingle() {
-    alert('   ');
-    document.getElementById('resultSingle').innerText = ' ';
-}
