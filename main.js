9e2c8af0ce606b68a3b93c5b08e7f03366c8e53b
hanching <mnshhps@gmail.com>
2026-09-19 14:30:25 +0800

    Feat: 完成 Modal 路線指引功能

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function showPage(pageId) {
     // 2. 顯示指定的頁面
     document.getElementById(pageId).classList.add('active-page');
 }
+// 顯示教室位置與走法
+function showLocation(course, room, route) {
+    document.getElementById('modalCourse').innerText = course;
+    document.getElementById('modalRoom').innerText = room;
+    document.getElementById('modalRoute').innerText = route;
+    document.getElementById('locationModal').style.display = 'flex';
+}
+
+// 關閉視窗
+function closeModal() {
+    document.getElementById('locationModal').style.display = 'none';
+}
